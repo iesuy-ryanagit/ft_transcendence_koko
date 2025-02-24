cb72525d199b9e291bb9ba995a116d7a7b2936a9
Yuki Hosono <hosono@YukinoMacBook-Pro.local>
2025-02-24 23:04:55 +0900

    feat: add game setting model and endpoints based in it

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-6vbvq6dager_4*mlch9dx3v3ti=p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'tournament',
+    'game',
 	'rest_framework',
     'rest_framework.authtoken',
 ]
