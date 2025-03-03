adbf7f80cffa562075db1df14eb1e74a8813ff97
柳田琉成 <iesuyryanagita@gmail.com>
2025-03-03 16:43:18 +0900

    fix model

--- account/user/models.py
+++ account/user/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     match_history = models.JSONField(default=list, blank=True)
-
+    otp_enabled = models.BooleanField(default=False)
     def __str__(self):
         return self.username
 
