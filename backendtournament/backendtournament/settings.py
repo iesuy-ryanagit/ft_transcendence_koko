09abc54ddeeb75172fff0cbf069c7dfeff958469
柳田琉成 <iesuyryanagita@gmail.com>
2025-03-04 17:07:21 +0900

    add cors in setting

--- backendtournament/backendtournament/settings.py
+++ backendtournament/backendtournament/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6vbvq6dager_4*mlch9dx3v3ti=p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 	'tournament',
 	'rest_framework',
     'rest_framework.authtoken',
@@ -49,6 +50,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
