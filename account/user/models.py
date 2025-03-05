faec254b79e3b586439f99ff9e59899b062f22ff
柳田琉成 <iesuyryanagita@gmail.com>
2025-03-05 13:31:31 +0900

    add setup-game to model

--- account/user/models.py
+++ account/user/models.py
@@ -5,16 +5,8 @@ class CustomUser(AbstractUser):
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     match_history = models.JSONField(default=list, blank=True)
     otp_enabled = models.BooleanField(default=False)
+    ball_speed = models.DecimalField(max_digits=5, decimal_places=2, default=1.0)
+    timer = models.PositiveIntegerField(default=60)
+
     def __str__(self):
         return self.username
-
-# class MatchResult(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='match_results')
-#     match_date = models.DateTimeField(auto_now_add=True)
-#     result = models.CharField(max_length=20)  # 勝利、敗北、引き分けなど
-#     score = models.CharField(max_length=20)  # 例: "10-5"
-
-#     def __str__(self):
-#         return f"{self.user.username} - {self.match_date} - {self.result}"
-
-
