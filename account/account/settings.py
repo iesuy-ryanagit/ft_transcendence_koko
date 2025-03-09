8f7344cf13874c0dba4a2e13b9cd2492d8d4d564
柳田琉成 <iesuyryanagita@gmail.com>
2025-03-09 17:52:57 +0900

    fix Dockerfile and plus connect db

--- account/account/settings.py
+++ account/account/settings.py
@@ -56,23 +56,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'account.wsgi.application'
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "PASSWORD": "postgres",
-#         "HOST": "db",
-#         "PORT": "5432",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # SQLite database file located in the base directory
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',  # SQLite database file located in the base directory
+#     }
+# }
 
 
 # Password validation
