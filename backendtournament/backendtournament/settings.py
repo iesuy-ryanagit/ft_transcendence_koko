71e98b7104a7d4593b92a98d7fb44b31256251fa
柳田琉成 <iesuyryanagita@gmail.com>
2025-03-14 20:31:26 +0900

    change db

--- backendtournament/backendtournament/settings.py
+++ backendtournament/backendtournament/settings.py
@@ -85,11 +85,22 @@ WSGI_APPLICATION = 'backendtournament.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "tournament_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
