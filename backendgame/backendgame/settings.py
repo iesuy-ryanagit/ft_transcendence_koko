377ed19fbbba769e04bb4421d42317266bb04585
柳田琉成 <iesuyryanagita@gmail.com>
2025-03-17 12:31:33 +0900

    fix settings in game

--- backendgame/backendgame/settings.py
+++ backendgame/backendgame/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6vbvq6dager_4*mlch9dx3v3ti=p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
