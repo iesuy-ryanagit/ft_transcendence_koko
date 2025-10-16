9559632f2b04f644de5c485a89c0baafe21bec61
柳田琉成 <iesuyryanagita@gmail.com>
2025-10-16 10:11:04 +0900

    fix

--- backendtournament/tournament/customuser.py
+++ backendtournament/tournament/customuser.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+class CustomUser(AbstractUser):
     match_history = models.JSONField(default=list, blank=True)
+    otp_enabled = models.BooleanField(default=False)
+    ball_speed = models.DecimalField(max_digits=5, decimal_places=2, default=1.0)
+    timer = models.PositiveIntegerField(default=60)
 
     def __str__(self):
         return self.username
